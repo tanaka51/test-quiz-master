bb8b8d6a3643d9f325d116a89b11070b3370ffb1
Seth Jeffery <seth@quipper.com>
2015-04-21 16:52:12 +0100

    first commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,42 @@
+class QuestionsController < ApplicationController
+  before_filter :find_question, only: [:show, :edit, :update]
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new question_params
+    if @question.save
+      redirect_to root_path, notice: 'Question created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update_attributes question_params
+      redirect_to root_path, notice: 'Question saved successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def answer
+    # TODO
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:question, :answer)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Question.create(question: "What is the capital of Japan?", answer: "Tokyo")
+Question.create(question: "What is 70 ÷ 10 ?", answer: "7")
+Question.create(question: "What is the closest planet to the Sun?", answer: "Mercury")
