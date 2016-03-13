9149aea143ef8f3cd333104f5f6c3cbd50c1f0d8
Koichi Tanaka <tanaka51.jp@gmail.com>
2016-03-13 22:29:08 +0900

    Try to be documentable spec using factory_girl
    `$ rspec --format documentation` shows more documentable output.
    But original code is more simpler than this...

--- rails/spec/models/question_spec.rb
+++ rails/spec/models/question_spec.rb
@@ -1,23 +1,59 @@
 require 'rails_helper'
 
 describe Question, :type => :model do
-  describe 'is_correct?' do
-    it 'requires the correct answer' do
-      expect(Question.new(answer: "Paris").is_correct?("London")).to be false
-      expect(Question.new(answer: "Paris").is_correct?("Paris")).to be true
+  describe '#is_correct?' do
+    subject { question.is_correct?(submission) }
+
+    let(:question) { build(:question, answer: answer) }
+
+    describe 'requires the correct answer' do
+      let(:answer) { 'Paris' }
+
+      context 'answer: "Paris", submission: "London"' do
+        let(:submission) { 'London' }
+
+        it { is_expected.to eq false }
+      end
+
+      context 'answer: "Paris", submission: "Paris"' do
+        let(:submission) { 'Paris' }
+
+        it { is_expected.to eq true }
+      end
     end
 
-    it 'ignores whitespace' do
-      expect(Question.new(answer: "  the  moon").is_correct?("the moon")).to be true
+    describe 'ignores whitespace' do
+      context 'answer:  "  the  moon", submission: "the moon"' do
+        let(:answer)     { "  the  moon" }
+        let(:submission) { "the moon" }
+
+        it { is_expected.to eq true }
+      end
     end
 
-    it 'ignores case' do
-      expect(Question.new(answer: "France").is_correct?("france")).to be true
+    describe 'ignores case' do
+      context 'answer: "France", submision: "france"' do
+        let(:answer)    { 'France' }
+        let(:submission) { 'france' }
+
+        it { is_expected.to eq true }
+      end
     end
 
-    it 'understands numbers as words' do
-      expect(Question.new(answer: "7").is_correct?("seven")).to be true
-      expect(Question.new(answer: "seven").is_correct?("7")).to be true
+    describe 'understands numbers as words' do
+      context 'answer: "7", submision: "seven"' do
+        let(:answer)    { '7' }
+        let(:submission) { 'seven' }
+
+        it { is_expected.to eq true }
+      end
+
+      context 'answer: "seven", submision: "7"' do
+        let(:answer)    { 'seven' }
+        let(:submission) { '7' }
+
+        it { is_expected.to eq true }
+      end
     end
   end
 end
